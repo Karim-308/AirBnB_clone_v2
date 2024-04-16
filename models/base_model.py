c381781bbf505277f3a1b49fa78aace3ec713a1c
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-16 22:00:35 +0200

    trying to fix kwargs not defined commint from init

--- models/base_model.py
+++ models/base_model.py
@@ -24,20 +24,24 @@ class BaseModel:
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
-
-        if len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        if kwargs:
+            if len(kwargs) == 0:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                regex_Str = "%Y-%m-%dT%H:%M:%S.%f"
+                for key, value in kwargs.items():
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, regex_Str))
+                    elif key == "__class__":
+                        continue
+                    else:
+                        setattr(self, key, value)
         else:
-            regex_Str = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, regex_Str))
-                elif key == "__class__":
-                    continue
-                else:
-                    setattr(self, key, value)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at      
 
     def __str__(self):
         """Returns a string representation of the instance"""
