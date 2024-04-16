2bec78d9d6067b023ff7ccb612f79d196a8dc384
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-16 04:50:40 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,17 +15,12 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            # Provide default values for 'updated_at' and 'created_at'
-            kwargs.setdefault('updated_at', datetime.now())
-            kwargs.setdefault('created_at', datetime.now())
+            # Convert ISO formatted strings to datetime objects
+            kwargs['updated_at'] = kwargs.get('updated_at', datetime.now())
+            kwargs['created_at'] = kwargs.get('created_at', datetime.now())
 
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                 '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                 '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
-
 
     def __str__(self):
         """Returns a string representation of the instance"""
