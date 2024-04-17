7fface2af34554b15fb43c5419aa9f745f9a3864
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-17 04:34:41 +0200

    solve task 6

--- models/base_model.py
+++ models/base_model.py
@@ -15,17 +15,20 @@ class BaseModel(declarative_base):
 
     def __init__(self, *args, **kwargs):
         '''the constructor'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
-            for key, value in kwargs.items():
-                if key in ["created_at", "updated_at"]:
-                    update_date = datetime.strptime(
-                        value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, update_date)
-                elif key != "__class__":
-                    setattr(self, key, value)
+            if len(kwargs) == 0:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            else:
+                regex_Str = "%Y-%m-%dT%H:%M:%S.%f"
+                for key, value in kwargs.items():
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, regex_Str))
+                    elif key == "__class__":
+                        continue
+                    else:
+                        setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
