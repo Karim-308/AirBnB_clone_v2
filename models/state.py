f5684390fd55fbd3a12cb42da12a9951a0eb8e5d
Karim-308 <2001118@eng.asu.edu.eg>
2024-04-17 21:04:35 +0200

    removing primary key from state.py

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
 
     if models.storage_t == "db":
         __tablename__ = 'states'
-        name = Column(String(128), nullable=False, primary_key=True)
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
     else:
         name = ""
