d9e36a291a2e8d016c077d934f6cde20851d2a32
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-17 04:15:26 +0200

    solve task 6

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''the constructor'''
-        if models.storage_t == "db":
-            id = Column(String(60), primary_key=True)
-            created_at = Column(DateTime, default=datetime.utcnow)
-            updated_at = Column(DateTime, default=datetime.utcnow)
-        elif kwargs:
+        id = Column(String(60), primary_key=True, nullable=False)
+        created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+        if kwargs:
             if len(kwargs) == 0:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
@@ -38,10 +37,6 @@ class BaseModel:
                         continue
                     else:
                         setattr(self, key, value)
-        else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.utcnow()
-                self.updated_at = self.created_at      
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -66,7 +61,7 @@ class BaseModel:
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
-   
-    def delete(self): 
+
+    def delete(self):
         """ delete the current instance from the storage """
         models.storage.delete(self)
