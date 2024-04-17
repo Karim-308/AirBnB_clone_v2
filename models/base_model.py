9e6465e4ed605b4e6bb3423778e9a9e1dd4015e3
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-04-17 02:31:34 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -24,21 +24,21 @@ class BaseModel:
             id = Column(String(60), primary_key=True)
             created_at = Column(DateTime, default=datetime.utcnow)
             updated_at = Column(DateTime, default=datetime.utcnow)
-            if kwargs:
-                if len(kwargs) == 0:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
-                else:
-                    regex_Str = "%Y-%m-%dT%H:%M:%S.%f"
-                    for key, value in kwargs.items():
-                        if key == "created_at" or key == "updated_at":
-                            setattr(self, key, datetime.strptime(value, regex_Str))
-                        elif key == "__class__":
-                            continue
-                        else:
-                            setattr(self, key, value)
+        elif kwargs:
+            if len(kwargs) == 0:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
             else:
+                regex_Str = "%Y-%m-%dT%H:%M:%S.%f"
+                for key, value in kwargs.items():
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, regex_Str))
+                    elif key == "__class__":
+                        continue
+                    else:
+                        setattr(self, key, value)
+        else:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.utcnow()
                 self.updated_at = self.created_at      
