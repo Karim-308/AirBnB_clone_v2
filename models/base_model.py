8c4f39b0f843314cee91722d9f34e576cb8a35eb
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-17 04:50:23 +0200

    first sol

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,10 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''the constructor'''
-        if models.storage_t == "db":
-            id = Column(String(60), primary_key=True)
-            created_at = Column(DateTime, default=datetime.utcnow)
-            updated_at = Column(DateTime, default=datetime.utcnow)
-        elif kwargs:
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
+        if kwargs:
             if len(kwargs) == 0:
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
