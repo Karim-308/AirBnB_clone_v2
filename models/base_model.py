b5db496bdd0ee308180f3a3b1b393e9e61c8c718
Abdullah-Faik <abdallahfaik80@gmail.com>
2024-04-17 04:33:22 +0200

    solve task 6

--- models/base_model.py
+++ models/base_model.py
@@ -7,29 +7,25 @@ from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 
-
 class BaseModel(declarative_base):
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         '''the constructor'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
-            if len(kwargs) == 0:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-            else:
-                regex_Str = "%Y-%m-%dT%H:%M:%S.%f"
-                for key, value in kwargs.items():
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, regex_Str))
-                    elif key == "__class__":
-                        continue
-                    else:
-                        setattr(self, key, value)
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    update_date = datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, update_date)
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
